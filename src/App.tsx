8af2ccb8cddb6179de4a6083c753b679d2f8ebae
Pavlo Zahryvyi <zagriviy2@gmail.com>
2025-08-28 22:43:23 +0000

    feat: login form, login logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
-function App() {
-  return <p>Hello</p>;
-}
+import type { FC } from "react";
+import { Login } from "./features/login/login";
+import { CssBaseline } from "@mui/material";
+import { USER_EMAIL_KEY, USER_PASSWORD_KEY } from "./constants";
+import { CoursesList } from "./features/courses-list/courses-list";
 
-export default App;
+export const App: FC = () => {
+  const userEmail = localStorage.getItem(USER_EMAIL_KEY);
+  const userPassword = localStorage.getItem(USER_PASSWORD_KEY);
+
+  return (
+    <>
+      <CssBaseline />
+      {userEmail && userPassword ? <CoursesList /> : <Login />}
+    </>
+  );
+};
---
+++ src/features/courses-list/courses-list.tsx
@@ -0,0 +1,5 @@
+import type { FC } from "react";
+
+export const CoursesList: FC = () => {
+  return <p>COURSES</p>;
+};
