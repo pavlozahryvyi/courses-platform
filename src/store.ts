d5371b961d10bbebb84836acba8a08b4c36fe03e
Pavlo Zahryvyi <zagriviy2@gmail.com>
2025-08-30 10:29:18 +0000

    feat: reset state logic

--- src/store.ts
+++ src/store.ts
@@ -1,18 +1,30 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore, type Action } from "@reduxjs/toolkit";
 import activeVideoReducer from "./store/active-video.slice";
-import authReducer from "./store/auth.slice";
+import authReducer, { clearUser } from "./store/auth.slice";
 import coursesSlice from "./store/courses.slice";
 import { coursesApi } from "./api/courses.api";
 import notificationSlice from "./store/notification.slice";
 
+const appReducer = combineReducers({
+  activeVideo: activeVideoReducer,
+  auth: authReducer,
+  courses: coursesSlice,
+  notification: notificationSlice,
+  [coursesApi.reducerPath]: coursesApi.reducer,
+});
+
+export type AppState = ReturnType<typeof appReducer>;
+
+const resettableRootReducer = (state: AppState | undefined, action: Action) => {
+  if (action.type === clearUser.type) {
+    return appReducer(undefined, action);
+  }
+
+  return appReducer(state, action);
+};
+
 export const store = configureStore({
-  reducer: {
-    activeVideo: activeVideoReducer,
-    auth: authReducer,
-    courses: coursesSlice,
-    notification: notificationSlice,
-    [coursesApi.reducerPath]: coursesApi.reducer,
-  },
+  reducer: resettableRootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(coursesApi.middleware),
 });
